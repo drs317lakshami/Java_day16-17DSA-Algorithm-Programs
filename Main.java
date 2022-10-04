5c824448f8857e4d4a295da8470bd0b769b67b5d
Devraj Kumar <109935648+drs317lakshami@users.noreply.github.com>
2022-10-05 00:38:25 +0530

    added find the number

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.java.day1617;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Permutations.permutation("abc");
+    BinarySearch.search();
+    InsertionSort.sort();
+    BubbleSort.sort();
+    MergeSort.sort();
+    Anagrams.check();
+    PrimeNumbers.find();
+    FindNumberGame.find();
+
+  }
+}
